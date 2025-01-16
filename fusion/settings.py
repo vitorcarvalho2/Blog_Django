25c615a8828e9c98268cf38505984dc361622d83
VitorCarvalho2 <vitorco.oliveira@gmail.com>
2025-01-16 20:19:13 -0300

    fix

--- fusion/settings.py
+++ fusion/settings.py
@@ -126,8 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
